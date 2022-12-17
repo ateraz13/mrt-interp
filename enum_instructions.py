d39139b8ebcacdecdf0412087c1dd273254f9567
polymer13 <mipsmartin@gmail.com>
2022-12-17 04:03:38 +0000

    Generate instuctions
    The scripts generates enumerations for opcodes, stores keywords for
    instructions in an array and declares callback functions for them.

--- enum_instructions.py
+++ enum_instructions.py
@@ -1,10 +1,56 @@
 import os
+import json
+import subprocess
 
-instructions = open("./instructions.txt", "r")
-enumeration = open("./instructions.txt.enum", "w")
+
+header_filename = "./instructions.hxx"
+data_json_filename = "./instructions.json"
+
+data = json.load(open(data_json_filename, "r"))
+header_file = open(header_filename, "w")
+
+hw = hw
 
 idx = 0
-for i in instructions:
-    i = i.replace("\n", "")
-    enumeration.write("static const uint8_t " + i + "= " + str(idx) + ";\n")
+hw("#ifndef INSTRUCTIONS_HXX\n")
+hw("#define INSTRUCTIONS_HXX\n")
+hw("#include <array>\n")
+hw("#include <cstdint> \n\n")
+hw("struct OpCodes {\n")
+
+for i in data["instructions"].keys():
+    # i = i.replace("\n", "")
+    hw("\tstatic const uint8_t " + i + " = " + str(idx) + ";\n")
     idx += 1
+
+hw("};\n\n")
+hw("std::array<const char*, " + str(len(data["instructions"].keys())) + "> instruction_keywords = {\n")
+
+for i in data["instructions"].keys():
+    hw("\t\"" + data["instructions"][i]["keyword"] + "\",\n")
+
+
+
+hw("};\n\n")
+
+hw("namespace callbacks {\n\n")
+
+for i in data["instructions"]:
+    keyword = data["instructions"][i]["keyword"]
+    hw("void " + keyword + "_cb" + "( VirtualMachine* vm")
+    for a in data["instructions"][i]["args"]:
+        match arg
+
+
+
+hw("#endif //INSTRUCTIONS_HXX")
+header_file.flush()
+header_file.close()
+
+# We might use stdin to send the code directly to the process and than let it write it to a file
+
+clang_format_cmd = "clang-format -i " + header_filename
+
+cres = subprocess.call(clang_format_cmd, shell = True)
+if not cres: print ("Clang format successful!")
+else: print("Clang format unsuccessful!")
