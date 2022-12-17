262012092f501895bbdcb704dd02fdac5db33c12
polymer13 <mipsmartin@gmail.com>
2022-12-17 08:05:27 +0000

    Parse and run instruction generated.
    Instead of manually implementing the parser for the bytecode the
    script handles this and generates the logic to parse a instruction and
    execute it.

--- enum_instructions.py
+++ enum_instructions.py
@@ -22,13 +22,33 @@ parameter_data_types = {
     "float" : "float"
 }
 
+parse_functions = {
+    "reg" : "read_valid_regid",
+    "fl_reg" : "read_valid_float_regid",
+    "addr" : "read_valid_mem_address",
+    "u8" : "read_valid_u8_immediate_val",
+    "u16" : "read_valid_u16_immediate_val",
+    "u32" : "read_valid_u32_immediate_val",
+    "i8" : "read_valid_i8_immediate_val",
+    "i16" : "read_valid_i16_immediate_val",
+    "i32" : "read_valid_i32_immediate_val",
+    "float" : "read_valid_float_immediate_val"
+}
+
 hw = header_file.write
 
 idx = 0
+
+# Header guards and includes
+
 hw("#ifndef INSTRUCTIONS_HXX\n")
 hw("#define INSTRUCTIONS_HXX\n")
 hw("#include <array>\n")
 hw("#include <cstdint> \n\n")
+
+
+# Generate enumerations for opcodes
+
 hw("struct OpCodes {\n")
 
 for i in data["instructions"].keys():
@@ -36,21 +56,25 @@ for i in data["instructions"].keys():
     hw("\tstatic const uint8_t " + i + " = " + str(idx) + ";\n")
     idx += 1
 
+
 hw("};\n\n")
+
+# Generate instruction keyword table
+
 hw("std::array<const char*, " + str(len(data["instructions"].keys())) + "> instruction_keywords = {\n")
 
 for i in data["instructions"].keys():
     hw("\t\"" + data["instructions"][i]["keyword"] + "\",\n")
 
-
-
 hw("};\n\n")
+
+# Generate declarations of callback functions that implement instruction logic
 
 hw("namespace callbacks {\n\n")
 
 for i in data["instructions"]:
     keyword = data["instructions"][i]["keyword"]
-    hw("\tvoid " + keyword + "_cb" + "( VirtualMachine* vm")
+    hw("\tvoid " + keyword + "_cb" + "( VirtualMachine& vm")
     for arg_name in data["instructions"][i]["args"].keys():
         arg_type = data["instructions"][i]["args"][arg_name]
         hw(", " + parameter_data_types[arg_type] + " " + arg_name)
@@ -58,6 +82,31 @@ for i in data["instructions"]:
     hw(");\n\n")
 
 hw("}\n\n")
+
+## Generate function that parses and executes the next instruction
+
+hw("void run_next_instruction (VirtualMachine &vm) {\n")
+
+hw("using MemBank = VirtualMachine::Interpreter::MemoryBank;\n")
+hw("auto& pc = vm.m_interp.m_mb[MemBank::PROGRAM_COUNTER_REG];\n")
+hw("auto& mem = vm.m_interp.m_mb.memory;\n")
+hw("uint8_t opcode = mem[pc];\n\n")
+hw("switch (opcode) {\n")
+
+for i in data["instructions"]:
+    hw("case " + i + ":\n")
+    instruction = data["instructions"][i]
+    for arg_name in instruction["args"]:
+        arg_type = instruction["args"][arg_name]
+        hw(parameter_data_types[arg_type] + " " + arg_name + " = " + parse_functions[arg_type] + "(mem, pc);\n")
+    hw(instruction["keyword"] + "_cb(vm")
+    for arg_name in instruction["args"]:
+        hw(", " + arg_name)
+
+    hw(");\nbreak;\n")
+
+hw("} // switch statement \ņ")
+hw("} // run_next_instruction \n\n")
 
 hw("#endif //INSTRUCTIONS_HXX")
 header_file.flush()
