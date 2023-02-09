5ed83576d3b5973e888cbd89d1a13793b25b9f14
polymer13 <mipsmartin@gmail.com>
2023-02-09 10:51:11 +0000

    Formating changes.

--- src/assembler/assembler.hxx
+++ src/assembler/assembler.hxx
@@ -1,6 +1,7 @@
 #ifndef ASSEMBLER_HXX
 #define ASSEMBLER_HXX
 #include <iostream>
+#include <string>
 #include <vector>
 
 namespace assembler {
@@ -10,13 +11,52 @@ class Label {
   uint32_t offset;
 };
 
+struct Token {
+  enum {
+    LABEL_SPECIFIER,
+    KEYWORD,
+    REGISTER_ID,
+    IMMEDIATE_VALUE,
+    MEMORY_ADDRESS
+  } type;
+
+  std::string value;
+};
+
+using TokenVector = std::vector<Token>;
+using MaybeTokenVector = std::optional<TokenVector>;
+
+class Tokenizer {
+  enum struct State {
+    STATEMENT_BEGINING,
+    FOUND_LABEL_COLUMN,
+    FOUND_KEYWORD,
+    EXPECTING_REGISTER_ID,
+    EXPECTING_IMMEDIATE_VALUE,
+    FOUND_DECIMAL,
+    READING_DECIMAL,
+    FOUND_ARGUMENT_SEPARATOR
+  };
+
+  void clear_errors();
+  void clear_all();
+
+private:
+  std::vector<std::string> errors;
+};
+
 class Assembler {
 public:
   Assembler &operator<<(const std::string &str);
 
+  MaybeTokenVector tokenize(const std::string &str);
+
+  const std::vector<std::string> get_errors() const { return errors; };
+
 private:
   std::vector<Label> m_labels;
   std::vector<uint8_t> m_bytecode;
+  Tokenizer m_tokinizer;
 };
 } // namespace assembler
 
--- src/syntax.hxx
+++ src/syntax.hxx
@@ -2,81 +2,94 @@
 #define SYNTAX_HXX
 #include "parse.hxx"
 #include <cstring>
+#include <iostream>
 #include <vector>
 
 /*
-  NOTE: Although the values are parsed during the tokenization, during the runtime
-  the values will be stored in memory and most likely the interpreter will hold references
-  to memory addresses where these values are stored, the memory bank is responsible for this task.
+  NOTE: Although the values are parsed during the tokenization, during the
+  runtime the values will be stored in memory and most likely the interpreter
+  will hold references to memory addresses where these values are stored, the
+  memory bank is responsible for this task.
 
-  Interpreter can be passed down into other classes in order its resources accessible to them.
+  Interpreter can be passed down into other classes in order its resources
+  accessible to them.
 */
+
+namespace generic_ling {
+
+class LineIterator {
+public:
+  LineIterator operator++();
+  LineIterator operator--();
+  const std::string &operator*() const;
+
+private:
+  std::istream input_strm;
+};
+
+} // namespace generic_ling
+
 namespace math_ling {
 
-  struct Value {
+struct Value {};
 
-  };
+struct Term {
+  std::vector<Value> values;
+};
 
-  struct Term {
-    std::vector<Value> values;
-  };
-
-  struct SetOfTerms {
-    std::vector<std::pair<bool, Term>> terms;
-  };
-}
+struct SetOfTerms {
+  std::vector<std::pair<bool, Term>> terms;
+};
+} // namespace math_ling
 
 struct TokenizeError {
 
-  enum Type {
-    Nothing = 0,
-    FailedParsing
-  } type;
-
+  enum Type { Nothing = 0, FailedParsing } type;
 };
 
 struct ExpressionToken {
-  enum Type {
-    NUMBER, OPERATOR, IDENTIFIER
-  } type;
+  enum Type { NUMBER, OPERATOR, IDENTIFIER } type;
 
   union Value {
     double number;
     MathOperator math_operator;
     // NOTE: All identifiers will be stored in a single block of memory
-    const char* identifier;
+    const char *identifier;
 
     Value() { std::memset(this, 0, sizeof(Value)); }
   } value;
 
-  static ExpressionToken MakeNumber( double val ) {
-    auto t = ExpressionToken { NUMBER, {} };
+  static ExpressionToken MakeNumber(double val) {
+    auto t = ExpressionToken{NUMBER, {}};
     t.value.number = val;
     return t;
   }
 
   static ExpressionToken MakeMathOperator(MathOperator op) {
-    auto t = ExpressionToken { OPERATOR, {} };
+    auto t = ExpressionToken{OPERATOR, {}};
     t.value.math_operator = op;
     return t;
   }
 
-  friend std::ostream& operator<<(std::ostream&, ExpressionToken);
+  friend std::ostream &operator<<(std::ostream &, ExpressionToken);
 };
 
-// I would much prefer for this function to be in the source file but MSVC seems to not like specialization of templates.
-// The program doesn't seem to link under MSVC if the implementation is in the source file.
-template<class Container>
-TokenizeError tokenize(CharIter begin_iter, CharIter end_iter, Container& output) {
+// I would much prefer for this function to be in the source file but MSVC seems
+// to not like specialization of templates. The program doesn't seem to link
+// under MSVC if the implementation is in the source file.
+template <class Container>
+TokenizeError tokenize(CharIter begin_iter, CharIter end_iter,
+                       Container &output) {
 
   enum TokenizeState {
-    LOOKING_FOR_NUMBER, LOOKING_FOR_OPERATOR
+    LOOKING_FOR_NUMBER,
+    LOOKING_FOR_OPERATOR
   } ts = LOOKING_FOR_NUMBER;
 
-  while(begin_iter != end_iter) {
+  while (begin_iter != end_iter) {
 
     try {
-      switch(ts) {
+      switch (ts) {
       case LOOKING_FOR_NUMBER: {
         auto res = parse_float(begin_iter, end_iter);
         begin_iter = res.second;
@@ -93,17 +106,18 @@ TokenizeError tokenize(CharIter begin_iter, CharIter end_iter, Container& output
         ts = LOOKING_FOR_NUMBER;
         continue;
       }
-      default: throw std::runtime_error("Invalid tokenize state!");
+      default:
+        throw std::runtime_error("Invalid tokenize state!");
       }
 
     } catch (ParseError pe) {
       std::cout << "Error while parsing: " << pe.what() << std::endl;
-      return TokenizeError { .type = TokenizeError::Type::FailedParsing };
+      return TokenizeError{.type = TokenizeError::Type::FailedParsing};
     }
   }
-  return TokenizeError { .type = TokenizeError::Type::Nothing };
+  return TokenizeError{.type = TokenizeError::Type::Nothing};
 }
 
-std::ostream& operator<<(std::ostream& out_strm, ExpressionToken expr);
+std::ostream &operator<<(std::ostream &out_strm, ExpressionToken expr);
 
 #endif // SYNTAX_HXX
