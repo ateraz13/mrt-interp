dd1bff58d9f32d874d531d1267bdbf5f75a8396d
polymer13 <mipsmartin@gmail.com>
2023-03-07 19:03:11 +0000

    Use macros to acccess MemoryBank.
    Instread of accessing fields in the memory bank we use
    macros to instead to make the code more readable and easier to
    maintain. Although macros can introduce some problems we can replace
    them in the future with functions and return references to variables or
    something similar.

--- src/interpreter.cxx
+++ src/interpreter.cxx
@@ -8,6 +8,20 @@
 
 #define DEBUG_EXTRA_INFO
 
+#define GP_REG(reg) interp.m_mb.gp_regs_32[reg]
+#define GP_REG_INFO(reg)                                                       \
+  "(R" << reg << " = " << interp.m_mb.gp_regs_32[reg] << ")"
+
+#define FL_REG(reg) interp.m_mb.fl_regs_32[reg]
+#define FL_REG_INFO(reg)                                                       \
+  "(R" << reg << " = " << interp.m_mb.fl_regs_32[reg] << ")"
+
+#define PC_REG interp.m_mb.gp_regs_32[MemoryBank::PROGRAM_COUNTER_REG]
+
+#define VM_MEMORY(addr) interp.m_mb.memory[addr]
+
+#define CHECK_FLAG(flag) interp.m_mb.check_flag(MemoryBank::flag)
+
 using OpCodes = VM::OpCodes;
 
 // NOTE: We need to deal with endienness in immediate values, memory addresses
@@ -18,6 +32,9 @@ using OpCodes = VM::OpCodes;
 // systems endianness since the vm stores numbers in little-endian format.
 
 void Interpreter::load_program(BytecodeBuffer &buffer) {
+#ifdef DEBUG_EXTRA_INFO
+  std::cout << "Loading program!\n";
+#endif
   if (buffer.size() > m_mb.memory.size()) {
     throw std::runtime_error(
         (boost::format("Program too large for VM memory!(size: %1%)") %
@@ -40,7 +57,8 @@ void Interpreter::run() {
   // FIXME: The program is loaded into memory, we're not using bytecode buffers.
   // They are only temporary storage used to store instructions before they are
   // passed to the interpreter.
-  auto &pc = m_mb.gp_regs_32[MemoryBank::PROGRAM_COUNTER_REG];
+  auto &interp = *this;
+  auto &pc = GP_REG(MemoryBank::PROGRAM_COUNTER_REG);
   auto &mem = m_mb.memory;
 
   try {
@@ -94,19 +112,16 @@ void VM::callbacks::popf_cb(Interpreter &interp,
 }
 
 void VM::callbacks::ld_cb(Interpreter &interp, const PL<OP::LOAD> &p) {
-  interp.m_mb.gp_regs_32[p.destination] =
-      *reinterpret_cast<uint32_t *>(&interp.m_mb.memory[p.source]);
+  GP_REG(p.destination) = *reinterpret_cast<uint32_t *>(&VM_MEMORY(p.source));
 }
 
 void VM::callbacks::lb_cb(Interpreter &interp, const PL<OP::LOAD_BYTE> &p) {
-  interp.m_mb.gp_regs_32[p.destination] =
-      *reinterpret_cast<uint8_t *>(&interp.m_mb.memory[p.source]);
+  GP_REG(p.destination) = *reinterpret_cast<uint8_t *>(&VM_MEMORY(p.source));
 }
 
 void VM::callbacks::lhw_cb(Interpreter &interp,
                            const PL<OP::LOAD_HALF_WORD> &p) {
-  interp.m_mb.gp_regs_32[p.destination] =
-      *reinterpret_cast<uint16_t *>(&interp.m_mb.memory[p.source]);
+  GP_REG(p.destination) = *reinterpret_cast<uint16_t *>(&VM_MEMORY(p.source));
 }
 
 void VM::callbacks::ldi_cb(Interpreter &interp,
@@ -115,217 +130,203 @@ void VM::callbacks::ldi_cb(Interpreter &interp,
   std::clog << "Loading immediate value (" << p.immediate_value
             << ") into register R" << (int)p.destination << std::endl;
 #endif
-  interp.m_mb.gp_regs_32[p.destination] = p.immediate_value;
+  GP_REG(p.destination) = p.immediate_value;
 }
 
 void VM::callbacks::lbi_cb(Interpreter &interp,
                            const PL<OP::LOAD_BYTE_IMMEDIATE> &p) {
-  interp.m_mb.gp_regs_32[p.destination] = p.immediate_value;
+  GP_REG(p.destination) = p.immediate_value;
 }
 
 void VM::callbacks::lhwi_cb(Interpreter &interp,
                             const PL<OP::LOAD_HALF_WORD_IMMEDIATE> &p) {
-  interp.m_mb.gp_regs_32[p.destination] = p.immediate_value;
+  GP_REG(p.destination) = p.immediate_value;
 }
 
 void VM::callbacks::lf_cb(Interpreter &interp, const PL<OP::LOAD_FLOAT> &p) {
-  interp.m_mb.fl_regs_32[p.destination] = interp.m_mb.memory[p.source];
+  FL_REG(p.destination) = VM_MEMORY(p.source);
 }
 
 void VM::callbacks::lfi_cb(Interpreter &interp,
                            const PL<OP::LOAD_FLOAT_IMMEDIATE> &p) {
-  interp.m_mb.fl_regs_32[p.destination] = p.immediate_value;
+  FL_REG(p.destination) = p.immediate_value;
 }
 
 void VM::callbacks::st_cb(Interpreter &interp, const PL<OP::STORE> &p) {
-  interp.m_mb.memory[p.destination] = interp.m_mb.gp_regs_32[p.source];
+  GP_REG(p.destination) = GP_REG(p.source);
 }
 
 void VM::callbacks::sb_cb(Interpreter &interp, const PL<OP::STORE_BYTE> &p) {
-  interp.m_mb.memory[p.destination] =
-      static_cast<uint8_t>(interp.m_mb.gp_regs_32[p.source] & 0xff);
+  VM_MEMORY(p.destination) = static_cast<uint8_t>(GP_REG(p.source) & 0xff);
 }
 
 void VM::callbacks::shw_cb(Interpreter &interp,
                            const PL<OP::STORE_HALF_WORD> &p) {
-  interp.m_mb.memory[p.destination] =
-      static_cast<uint16_t>(interp.m_mb.gp_regs_32[p.source] & 0xffff);
+  VM_MEMORY(p.destination) = static_cast<uint16_t>(GP_REG(p.source) & 0xffff);
 }
 
 void VM::callbacks::sf_cb(Interpreter &interp, const PL<OP::STORE_FLOAT> &p) {
-  interp.m_mb.memory[p.destination] = interp.m_mb.fl_regs_32[p.source];
+  VM_MEMORY(p.destination) = FL_REG(p.source);
 }
 
 void VM::callbacks::sll_cb(Interpreter &interp, const PL<OP::SHIFT_LEFT> &p) {
-  interp.m_mb.gp_regs_32[p.destination] = interp.m_mb.gp_regs_32[p.source]
-                                          << interp.m_mb.gp_regs_32[p.shift_by];
+  GP_REG(p.destination) = GP_REG(p.source) << GP_REG(p.shift_by);
 }
 
 void VM::callbacks::srl_cb(Interpreter &interp, const PL<OP::SHIFT_RIGHT> &p) {
-  interp.m_mb.gp_regs_32[p.destination] =
-      interp.m_mb.gp_regs_32[p.source] >> interp.m_mb.gp_regs_32[p.shift_by];
+  GP_REG(p.destination) = GP_REG(p.source) >> GP_REG(p.shift_by);
 }
 
 void VM::callbacks::slli_cb(Interpreter &interp,
                             const PL<OP::SHIFT_IMMEDIATE_LEFT> &p) {
-  interp.m_mb.gp_regs_32[p.destination] = interp.m_mb.gp_regs_32[p.source]
-                                          << p.shift_by;
+  GP_REG(p.destination) = GP_REG(p.source) << p.shift_by;
 }
 
 void VM::callbacks::srli_cb(Interpreter &interp,
                             const PL<OP::SHIFT_IMMEDIATE_RIGHT> &p) {
-  interp.m_mb.gp_regs_32[p.destination] =
-      interp.m_mb.gp_regs_32[p.source] >> p.shift_by;
+  GP_REG(p.destination) = GP_REG(p.source) >> p.shift_by;
 }
 
 void VM::callbacks::add_cb(Interpreter &interp, const PL<OP::ADD_INT> &p) {
-  interp.m_mb.gp_regs_32[p.destination] =
-      interp.m_mb.gp_regs_32[p.source1] + interp.m_mb.gp_regs_32[p.source2];
+  GP_REG(p.destination) = GP_REG(p.source1) + GP_REG(p.source2);
 }
 
 void VM::callbacks::addi_cb(Interpreter &interp,
                             const PL<OP::ADD_INT_IMMEDIATE> &p) {
-  interp.m_mb.gp_regs_32[p.destination] =
-      interp.m_mb.gp_regs_32[p.source] + p.immediate_value;
+  GP_REG(p.destination) = GP_REG(p.source) + p.immediate_value;
 }
 
 void VM::callbacks::sub_cb(Interpreter &interp, const PL<OP::SUB_INT> &p) {
-  interp.m_mb.gp_regs_32[p.destination] =
-      interp.m_mb.gp_regs_32[p.source1] - interp.m_mb.gp_regs_32[p.source2];
+  GP_REG(p.destination) = GP_REG(p.source1) - GP_REG(p.source2);
 }
 
 void VM::callbacks::subi_cb(Interpreter &interp,
                             const PL<OP::SUB_INT_IMMEDIATE> &p) {
-  interp.m_mb.gp_regs_32[p.destination] =
-      interp.m_mb.gp_regs_32[p.source] - p.immediate_value;
+  GP_REG(p.destination) = GP_REG(p.source) - p.immediate_value;
 }
 
 void VM::callbacks::mul_cb(Interpreter &interp, const PL<OP::MULT_INT> &p) {
-  interp.m_mb.gp_regs_32[p.destination] =
-      interp.m_mb.gp_regs_32[p.source1] * interp.m_mb.gp_regs_32[p.source2];
+  GP_REG(p.destination) = GP_REG(p.source1) * GP_REG(p.source2);
 }
 
 void VM::callbacks::muli_cb(Interpreter &interp,
                             const PL<OP::MULT_INT_IMMEDIATE> &p) {
-  interp.m_mb.gp_regs_32[p.destination] =
-      interp.m_mb.gp_regs_32[p.source] * p.immediate_value;
+  GP_REG(p.destination) = GP_REG(p.source) * p.immediate_value;
 }
 
 void VM::callbacks::div_cb(Interpreter &interp, const PL<OP::DIV_INT> &p) {
-  interp.m_mb.gp_regs_32[p.destination] =
-      interp.m_mb.gp_regs_32[p.source1] / interp.m_mb.gp_regs_32[p.source2];
+  GP_REG(p.destination) = GP_REG(p.source1) / GP_REG(p.source2);
 }
 
 void VM::callbacks::divi_cb(Interpreter &interp,
                             const PL<OP::DIV_INT_IMMEDIATE> &p) {
-  interp.m_mb.gp_regs_32[p.destination] =
-      interp.m_mb.gp_regs_32[p.source] / p.immediate_value;
+  GP_REG(p.destination) = GP_REG(p.source) / p.immediate_value;
 }
 
 void VM::callbacks::addf_cb(Interpreter &interp, const PL<OP::ADD_FLOAT> &p) {
-  interp.m_mb.fl_regs_32[p.destination] =
-      interp.m_mb.fl_regs_32[p.source1] + interp.m_mb.fl_regs_32[p.source2];
+  FL_REG(p.destination) = FL_REG(p.source1) + FL_REG(p.source2);
 }
 
 void VM::callbacks::addif_cb(Interpreter &interp,
                              const PL<OP::ADD_FLOAT_IMMEDIATE> &p) {
-  interp.m_mb.fl_regs_32[p.destination] =
-      interp.m_mb.fl_regs_32[p.source] + p.immediate_value;
+  FL_REG(p.destination) = FL_REG(p.source) + p.immediate_value;
 }
 
 void VM::callbacks::subf_cb(Interpreter &interp, const PL<OP::SUB_FLOAT> &p) {
-  interp.m_mb.fl_regs_32[p.destination] =
-      interp.m_mb.fl_regs_32[p.source1] - interp.m_mb.fl_regs_32[p.source2];
+  FL_REG(p.destination) = FL_REG(p.source1) - FL_REG(p.source2);
 }
 
 void VM::callbacks::subif_cb(Interpreter &interp,
                              const PL<OP::SUB_FLOAT_IMMEDIATE> &p) {
-  interp.m_mb.fl_regs_32[p.destination] =
-      interp.m_mb.fl_regs_32[p.source] - p.immediate_value;
+  FL_REG(p.destination) = FL_REG(p.source) - p.immediate_value;
 }
 
 void VM::callbacks::mulf_cb(Interpreter &interp, const PL<OP::MULT_FLOAT> &p) {
-  interp.m_mb.fl_regs_32[p.destination] =
-      interp.m_mb.fl_regs_32[p.source1] * interp.m_mb.fl_regs_32[p.source2];
+  FL_REG(p.destination) = FL_REG(p.source1) * FL_REG(p.source2);
 }
 
 void VM::callbacks::mulif_cb(Interpreter &interp,
                              const PL<OP::MULT_FLOAT_IMMEDIATE> &p) {
-  interp.m_mb.fl_regs_32[p.destination] =
-      interp.m_mb.fl_regs_32[p.source] * p.immediate_value;
+  FL_REG(p.destination) = FL_REG(p.source) * p.immediate_value;
 }
 
 void VM::callbacks::divf_cb(Interpreter &interp, const PL<OP::DIV_FLOAT> &p) {
-  interp.m_mb.fl_regs_32[p.destination] =
-      interp.m_mb.fl_regs_32[p.source1] / interp.m_mb.fl_regs_32[p.source2];
+#ifdef DEBUG_EXTRA_INFO
+  std::clog << "Dividing two float registers " << FL_REG_INFO(p.source1)
+            << " and " << FL_REG_INFO(p.source2) << "\n";
+#endif
+  FL_REG(p.destination) = FL_REG(p.source1) / FL_REG(p.source2);
 }
 
 void VM::callbacks::divif_cb(Interpreter &interp,
                              const PL<OP::DIV_FLOAT_IMMEDIATE> &p) {
-  interp.m_mb.fl_regs_32[p.destination] =
-      interp.m_mb.fl_regs_32[p.source] / p.immediate_value;
+#ifdef DEBUG_EXTRA_INFO
+  std::clog << "Dividing float register with float immediate (R" << p.source
+            << " = " << FL_REG(p.source) << ") and " << p.immediate_value
+            << "\n.";
+  std::clog << "Destination register R" << p.destination << ".\n";
+#endif
+  FL_REG(p.destination) = FL_REG(p.source) / p.immediate_value;
 }
 
 void VM::callbacks::jmp_cb(Interpreter &interp, const PL<OP::JUMP> &p) {
   check_mem_address_with_throw(p.jump_address);
-  interp.m_mb.gp_regs_32[MemoryBank::PROGRAM_COUNTER_REG] = p.jump_address;
+#ifdef DEBUG_EXTRA_INFO
+  std::clog << "Jumping to immediate value address (" << p.jump_address
+            << ")\n";
+#endif
+  PC_REG = p.jump_address;
 }
 
 // NOTE: Jump zero is the same thing as jump equal because the comparison uses
 // subtraction to compare two values and if the result is zero that means the
 // values are equal.
 void VM::callbacks::jz_cb(Interpreter &interp, const PL<OP::JUMP_ZERO> &p) {
-  if (interp.m_mb.check_flag(MemoryBank::ZERO_FLAG_BIT)) {
+  if (CHECK_FLAG(ZERO_FLAG_BIT)) {
     jmp_cb(interp, {p.jump_address});
   }
 }
 
 void VM::callbacks::jzr_cb(Interpreter &interp,
                            const PL<OP::JUMP_REGISTER_ZERO> &p) {
-  jz_cb(interp, {interp.m_mb.gp_regs_32[p.jump_register]});
+  jz_cb(interp, {GP_REG(p.jump_register)});
 }
 
 void VM::callbacks::je_cb(Interpreter &interp, const PL<OP::JUMP_EQUAL> &p) {
-  if (interp.m_mb.check_flag(MemoryBank::ZERO_FLAG_BIT)) {
+  if (CHECK_FLAG(ZERO_FLAG_BIT)) {
     jmp_cb(interp, {p.jump_address});
   }
 }
 
 void VM::callbacks::jer_cb(Interpreter &interp,
                            const PL<OP::JUMP_REGISTER_EQUAL> &p) {
-  je_cb(interp, {interp.m_mb.gp_regs_32[p.jump_register]});
+  je_cb(interp, {GP_REG(p.jump_register)});
 }
 
 void VM::callbacks::jlt_cb(Interpreter &interp,
                            const PL<OP::JUMP_LESS_THAN> &p) {
-  if (interp.m_mb.check_flag(MemoryBank::ZERO_FLAG_BIT) ||
-      interp.m_mb.check_flag(MemoryBank::SIGN_FLAG_BIT) !=
-          interp.m_mb.check_flag(MemoryBank::OVERFLOW_FLAG_BIT)) {
+  if (CHECK_FLAG(ZERO_FLAG_BIT) ||
+      CHECK_FLAG(SIGN_FLAG_BIT) != CHECK_FLAG(OVERFLOW_FLAG_BIT)) {
     jmp_cb(interp, {p.jump_address});
   }
 }
 
 void VM::callbacks::jltr_cb(Interpreter &interp,
                             const PL<OP::JUMP_REGISTER_LESS_THAN> &p) {
-  jlt_cb(interp, {interp.m_mb.gp_regs_32[p.jump_register]});
+  jlt_cb(interp, {GP_REG(p.jump_register)});
 }
 
 void VM::callbacks::jgt_cb(Interpreter &interp,
                            const PL<OP::JUMP_GREATER_THAN> &p) {
-  if (interp.m_mb.check_flag(MemoryBank::ZERO_FLAG_BIT) &&
-      interp.m_mb.check_flag(MemoryBank::SIGN_FLAG_BIT) ==
-          interp.m_mb.check_flag(MemoryBank::OVERFLOW_FLAG_BIT)) {
+  if (CHECK_FLAG(ZERO_FLAG_BIT) &&
+      CHECK_FLAG(SIGN_FLAG_BIT) == CHECK_FLAG(OVERFLOW_FLAG_BIT)) {
     jmp_cb(interp, {p.jump_address});
   }
 }
 
 void VM::callbacks::jgtr_cb(Interpreter &interp,
                             const PL<OP::JUMP_REGISTER_GREATER_THAN> &p) {
-  jgt_cb(interp, {interp.m_mb.gp_regs_32[p.jump_register]});
-}
-
-void VM::callbacks::halt_cb(Interpreter &interp, const PL<OP::HALT> &) {
-  interp.stop();
+  jgt_cb(interp, {GP_REG(p.jump_register)});
 }
 
 // NOTE: In order to load jump to instruction we need to copy the instructions
@@ -333,9 +334,19 @@ void VM::callbacks::halt_cb(Interpreter &interp, const PL<OP::HALT> &) {
 
 void VM::callbacks::jmpr_cb(Interpreter &interp,
                             const PL<OP::JUMP_REGISTER> &p) {
-  check_mem_address_with_throw(interp.m_mb.gp_regs_32[p.jump_register]);
-  interp.m_mb.gp_regs_32[MemoryBank::PROGRAM_COUNTER_REG] =
-      interp.m_mb.gp_regs_32[p.jump_register];
+  check_mem_address_with_throw(GP_REG(p.jump_register));
+#ifdef DEBUG_EXTRA_INFO
+  std::clog << "Jumping to address stored in register (R" << p.jump_register
+            << " = " << GP_REG(p.jump_register) << ").\n";
+#endif
+  PC_REG = GP_REG(p.jump_register);
+}
+
+void VM::callbacks::halt_cb(Interpreter &interp, const PL<OP::HALT> &) {
+#ifdef DEBUG_EXTRA_INFO
+  std::clog << "Halting the machine.\n";
+#endif
+  interp.stop();
 }
 
 // NOTE: Jump zero is the same thing as jump equal because the comparison uses
@@ -354,8 +365,8 @@ void VM::callbacks::cmp_cb(Interpreter &interp, const PL<OP::COMPARE> &p) {
             << "R" << (int)p.register1 << " and "
             << "R" << (int)p.register2 << std::endl;
 #endif
-  auto v1 = interp.m_mb.gp_regs_32[p.register1];
-  auto v2 = interp.m_mb.gp_regs_32[p.register2];
+  auto v1 = GP_REG(p.register1);
+  auto v2 = GP_REG(p.register2);
 
   // NOTE: We are using signed integer, so how the hell is it suppose to be less
   // than zero. See this is why you stop and think.
@@ -386,16 +397,30 @@ void VM::callbacks::cmp_cb(Interpreter &interp, const PL<OP::COMPARE> &p) {
 
 void VM::callbacks::cmpf_cb(Interpreter &interp,
                             const PL<OP::COMPARE_FLOAT> &p) {
-  float v1 = interp.m_mb.fl_regs_32[p.register1];
-  float v2 = interp.m_mb.fl_regs_32[p.register2];
+  float v1 = FL_REG(p.register1);
+  float v2 = FL_REG(p.register2);
+
+#ifdef DEBUG_EXTRA_INFO
+  std::clog << "Coparing two float registers R" << p.register1 << "and R"
+            << p.register2 << "\n";
+#endif
 
   if (v1 == v2) {
+#ifdef DEBUG_EXTRA_INFO
+    std::clog << v1 << " = " << v2 << "\n";
+#endif
     interp.m_mb.set_flag(MemoryBank::ZERO_FLAG_BIT);
     interp.m_mb.unset_flag(MemoryBank::SIGN_FLAG_BIT);
   } else if (v1 > v2) {
+#ifdef DEBUG_EXTRA_INFO
+    std::clog << v1 << " > " << v2 << "\n";
+#endif
     interp.m_mb.unset_flag(MemoryBank::ZERO_FLAG_BIT);
     interp.m_mb.set_flag(MemoryBank::SIGN_FLAG_BIT);
   } else {
+#ifdef DEBUG_EXTRA_INFO
+    std::clog << v1 << " < " << v2 << "\n";
+#endif
     interp.m_mb.unset_flag(MemoryBank::ZERO_FLAG_BIT);
     interp.m_mb.unset_flag(MemoryBank::SIGN_FLAG_BIT);
   }
@@ -435,7 +460,9 @@ RegID read_valid_float_regid(MemoryBank::MemoryBuffer &buffer, uint32_t &pc) {
 MemPtr read_valid_mem_address(MemoryBank::MemoryBuffer &buffer, uint32_t &pc) {
   check_bytes_ahead(buffer, pc, sizeof(MemPtr));
   auto mem_ptr = *reinterpret_cast<MemPtr *>(&buffer[pc]);
-  // std::cout << "MEM ADDRESS READ: " << mem_ptr << std::endl;
+#ifdef DEBUG_EXTRA_INFO
+  std::clog << "Reading Memory Address: " << mem_ptr << std::endl;
+#endif
   return mem_ptr;
 }
 
